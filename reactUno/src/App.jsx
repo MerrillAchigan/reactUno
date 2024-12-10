11527932bfa8ee1c6c93c6c591703e4a1bc35070
MerrillAchigan <87760869+MerrillAchigan@users.noreply.github.com>
2024-12-10 16:12:44 +0100

    first react lines

---
+++ reactUno/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./Header"
+import Food from "./Food";
+import Card from "./Card";
+import Footer from "./Footer";
+
+function App() {
+  return(
+    <>
+      <Header></Header>
+      <Food></Food>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App
---
+++ reactUno/src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile pic" />
+            <h2 className="card-title">Juicee</h2>
+            <p className="card-text">I am a software engineer and play video games</p>
+        </div>
+    )
+}
+
+export default Card
